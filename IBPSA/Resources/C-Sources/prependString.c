69630ffa1bce3d4a967c3e23c236547a234eb7ff
Filip Jorissen <filip.jorissen@kuleuven.be>
2018-05-25 08:33:19 +0200

    changed comments to c comments

--- IBPSA/Resources/C-Sources/prependString.c
+++ IBPSA/Resources/C-Sources/prependString.c
@@ -4,7 +4,7 @@
  */
 
 void* prependString(const char* fileName, const char* string){
-  // read original file contents
+  /* read original file contents */
   FILE *fr = fopen(fileName, "r");
   if(fseek(fr, 0, SEEK_END)!=0)
   	ModelicaFormatError("The file %s could not be read.", fileName);
@@ -16,7 +16,7 @@ void* prependString(const char* fileName, const char* string){
 
   char *origString = malloc(fsize + 1);
   if ( origString == NULL ){
-    // not enough memory is available: file too large
+    /* not enough memory is available: file too large */
     ModelicaError("Not enough memory in fileWriterInit.c for prepending string.");
   } 
   if (fread(origString, fsize, 1, fr)==0)
@@ -25,12 +25,12 @@ void* prependString(const char* fileName, const char* string){
   fclose(fr);
   origString[fsize] = '\0';
 
-  // write new contents
+  /* write new contents */
   FILE *fw = fopen(fileName, "w");
-  // prepended string
+  /* prepended string */
   if (fputs(string, fw)==EOF)
     ModelicaFormatError("The file %s could not be written.", fileName);
-  // original data
+  /* original data */
   if (fputs(origString, fw)==EOF)
     ModelicaFormatError("The file %s could not be written.", fileName);  
 
