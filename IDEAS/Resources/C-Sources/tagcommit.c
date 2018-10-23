faf22de78feb3e757fbbb6d6602b956bb52811c0
Filip Jorissen <filip.jorissen@kuleuven.be>
2018-10-23 21:53:09 +0200

    sha logging functionality for #940

---
+++ IDEAS/Resources/C-Sources/tagcommit.c
@@ -0,0 +1,60 @@
+/* Functions for getching git sha's and printing them to file.
+ *
+ * Filip Jorissen, KU Leuven				2018-10-23
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>	
+
+/* Fetch the sha of a library in the folder 'path' and return it in 'res'. */
+void getCommit(const char *path, char* res){
+  /* Buffers */
+  char buffer[250]; 
+  char sha[42];
+  /* Command to fetch sha */
+  sprintf(buffer, "cd %s && git rev-parse HEAD", path); 
+
+  /* Read command result */
+  FILE *fp;
+  fp = popen(buffer, "r");
+  if (fp == NULL) {
+    ModelicaFormatError("Failed to call git command\n");
+  }
+  if (fgets(sha, sizeof(sha)-1, fp) == NULL){
+  	ModelicaFormatError("Path %s does not exist or git cannot be called\n", path);
+  }
+  pclose(fp);
+
+  /* Return result */
+  strcpy(res,sha);
+}
+
+
+/* Fetch sha's for the libraries at locations 'libPaths' and print them to the file 'fileName'. */
+void tagCommit(const char *fileName, const char **libNames, const char **libPaths, int nLib, int verbose){ 
+	/* Buffers */
+	char sha[44];
+	char buffer[250]; 
+
+	/* Write header */
+	FILE *fOut = fopen(fileName, "w");
+	sprintf(buffer, "Library: sha\n---------------------------\n"); 
+	if (fputs(buffer, fOut)==EOF){
+		ModelicaFormatError("In tagcommit.c: failed to write to file %s.", fileName);
+ 	}
+
+ 	/* Fetch library sha's and print to file */
+ 	int i;
+	for (i = 0; i < nLib; ++i)
+	{
+		getCommit(libPaths[i], sha);
+		sprintf(buffer, "%s: %s\n", libNames[i],sha); 
+		if (verbose)
+			ModelicaFormatMessage("Library %s has sha %s. Writing to file path %s\n", libNames[i], sha, fileName);
+		if (fputs(buffer, fOut)==EOF){
+			ModelicaFormatError("In tagcommit.c: failed to write to file %s.", fileName);
+ 		}
+	}
+	fclose(fOut);
+}
